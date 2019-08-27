0636d8595de960ecce805cff116f00e9493c8419
Alexis Holguin <wholguinmor@uniminuto.edu.co>
2019-08-27 14:39:50 -0500

    cotizador de cajas

--- model/ItemBox.php
+++ model/ItemBox.php
@@ -2,8 +2,13 @@
 require_once __DIR__ . "/Item.php";
 class ItemBox extends Item implements JsonSerializable
 {
-    public function calculatePrice()
-    { 
-        return 5;
+  public function calculatePrice()
+  {
+    $directCost =  $this->getMaterial()->getPrice() / $this->getMeasurement()->getWindow();
+    if ($this->getQuantity() < 11000) {
+      return $directCost * 2;
+    } else {
+      return $directCost * 2.5;
     }
+  }
 }
