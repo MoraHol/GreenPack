de8cd590284fe986e4bd07fb60c2ec59b1c14378
Alexis Holguin <wholguinmor@uniminuto.edu.co>
2019-12-02 13:09:36 -0500

    bug

--- admin/partials/verify-session.php
+++ admin/partials/verify-session.php
@@ -1,10 +1,8 @@
-
 <?php
 /*
 *Desarrollada por Alexis Holguin(github: MoraHol)
 */
 if (session_status() == PHP_SESSION_NONE) {
-    session_start();
     if (!isset($_SESSION["admin"])) {
         header("Location: /admin");
     }
