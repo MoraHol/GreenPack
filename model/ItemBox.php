5d9279b2ebebf1e350567854376084f99b49b3cb
alexis holguin <wholguinmor@uniminuto.edu.co>
2019-12-06 10:37:23 -0500

    bug al momento de cotizar

--- model/ItemBox.php
+++ model/ItemBox.php
@@ -17,7 +17,7 @@ class ItemBox extends Item implements JsonSerializable
     $directCost =  $this->calculateDirectCost();
     if ($this->getQuantity() < 6000) {
       $this->setPrice($directCost * 4);
-    } else if($this->getQuantity() > 6000 && $this->getQuantity() < 10000) {
+    } else if($this->getQuantity() >= 6000 && $this->getQuantity() < 10000) {
       return $this->setPrice($directCost * 3);
     }else{
       return $this->setPrice($directCost * 2.5);
