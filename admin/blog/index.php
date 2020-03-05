ce4dcdac5ae0a5353babdb8ef6639cba7850ec8f
Sergio Eduardo Velandia <sergio.velandia@teenus.com.co>
2020-03-04 23:09:04 -0500

    Actualizacion titles

--- admin/blog/index.php
+++ admin/blog/index.php
@@ -11,7 +11,7 @@ $conversorDate = new ConversorDate();
 ?>
 
 <head>
-  <title>GreenPack | Noticias</title>
+  <title>Greenpack | Noticias</title>
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
