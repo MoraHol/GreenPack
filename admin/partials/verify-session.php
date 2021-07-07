2372c41c5ff861a165346f0f61ac306cb9e461e0
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2021-07-07 12:17:18 -0500

    fix sesion cotizaciones

--- admin/partials/verify-session.php
+++ admin/partials/verify-session.php
@@ -1,5 +1,9 @@
 <?php
-session_start();
+
+if (session_status() !== PHP_SESSION_ACTIVE)
+    session_start();
+
+//session_start();
 
 if (!isset($_SESSION)) {
     if (!isset($_SESSION["admin"])) {
