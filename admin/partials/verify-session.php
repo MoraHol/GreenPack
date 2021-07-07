2070c6b7099e4694e7f8dc07bdfdd5087996357e
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2021-07-07 13:16:43 -0500

    fix headers

--- admin/partials/verify-session.php
+++ admin/partials/verify-session.php
@@ -1,13 +1,11 @@
 <?php
 
 // Desarrollada por Teenus SAS
-if (!headers_sent())
-    if (!isset($_SESSION))
-        session_start();
-
 
 if (!isset($_SESSION)) {
+    session_start();
     if (!isset($_SESSION["admin"])) {
         header("Location: /admin");
     }
 }
+?>
